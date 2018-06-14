e505ccd9ad9116f7d709aef790f20583e47f1819
LeftHandFreeStudio <kawka.maciej.93@gmail.com>
2018-06-14 16:04:03 +0200

    added main class

---
+++ TemperatureConverterApp.java
@@ -0,0 +1,6 @@
+public class TemperatureConverterApp{
+	public static void main(String[] args){
+		System.out.println("Enter input temperature unit: ")
+		System.out.println("(Use K for Kelvin, C for Celsius, F for Fahrenheit)")
+	}
+}
