17ace292c1581bde492fba33c96b5a82f83b8631
LeftHandFreeStudio <kawka.maciej.93@gmail.com>
2018-06-15 08:19:22 +0200

    added logic controller

---
+++ temperatureConverterApp/src/main/java/com/lefthandfreestudio/LogicController.java
@@ -0,0 +1,16 @@
+package com.lefthandfreestudio;
+
+import java.util.Scanner;
+
+public class LogicController 
+{
+	private UserInputValidator inputValidator;
+	private Scanner userInput;
+	public LogicController(){
+		inputValidator = new UserInputValidator()
+		userInput = new Scanner(System.in);
+	}
+	public void communicateWithUser(){
+	
+	}
+}
