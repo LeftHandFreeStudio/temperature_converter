2d709025721c47eb80e504e6804dbdb737a07c18
LeftHandFreeStudio <kawka.maciej.93@gmail.com>
2018-06-15 07:55:05 +0200

    generated maven project

--- TemperatureConverterApp.java
+++
@@ -1,6 +0,0 @@
-public class TemperatureConverterApp{
-	public static void main(String[] args){
-		System.out.println("Enter input temperature unit: ")
-		System.out.println("(Use K for Kelvin, C for Celsius, F for Fahrenheit)")
-	}
-}
